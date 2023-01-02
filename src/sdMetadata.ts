5fa2b1f89a9da16b2e0be478a59c0e9bafef5ab9
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-01-02 18:20:38 +0800

    feat(sd-metadata): support A1111 in addition to InvokeAI

--- src/sdMetadata.ts
+++ src/sdMetadata.ts
@@ -52,54 +52,114 @@ export const parseSDMetadata = async (e: Message<boolean>) => {
   const data = await parse(funnyPath);
   await rm(funnyPath);
 
-  if (!data || !data['sd-metadata']) return;
-  const sdMetadata = JSON.parse(data['sd-metadata']) as SDMetadata;
+  if (!data) return;
 
-  await e.reply({
-    embeds: [
-      new EmbedBuilder()
-        .setTitle('Stable Diffusion metadata')
-        .setFields(
-          {
-            name: 'Model',
-            value: sdMetadata.model_weights,
-          },
-          {
-            name: 'Prompt',
-            value: sdMetadata.image.prompt![0].prompt,
-          },
-          {
-            name: 'Seed',
-            value: `${sdMetadata.image.seed}`,
-          },
-          {
-            name: 'Postprocessing',
-            value:
-              sdMetadata.image.postprocessing
-                ?.map(
-                  (k) =>
-                    `${k.type}${k.strength ? ` ${k.strength}` : ''}${
-                      k.scale ? ` ${k.scale}x` : ''
-                    }`
-                )
-                .join('\n') ?? 'None detected',
-          },
-          {
-            name: 'Sampler',
-            value: `${sdMetadata.image.sampler}`,
-            inline: true,
-          },
-          {
-            name: 'Steps',
-            value: `${sdMetadata.image.steps}`,
-            inline: true,
-          }
-        )
-        .setThumbnail(png.url)
-        .setFooter({
-          text: `Generated with ${sdMetadata.app_id} ${sdMetadata.app_version}`,
-        })
-        .setColor(0x38bdf8),
-    ],
-  });
+  if (data['sd-metadata']) {
+    const sdMetadata = JSON.parse(data['sd-metadata']) as SDMetadata;
+
+    await e.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle('Stable Diffusion metadata')
+          .setFields(
+            {
+              name: 'Model',
+              value: `${sdMetadata.model_weights} [${sdMetadata.model_hash}]`,
+            },
+            {
+              name: 'Prompt',
+              value: sdMetadata.image.prompt![0].prompt,
+            },
+            {
+              name: 'Size',
+              value: `${sdMetadata.image.width}x${sdMetadata.image.height}`,
+              inline: true,
+            },
+            {
+              name: 'Seed',
+              value: `${sdMetadata.image.seed}`,
+              inline: true,
+            },
+            {
+              name: 'Sampler',
+              value: `${sdMetadata.image.sampler}`,
+              inline: true,
+            },
+            {
+              name: 'Steps',
+              value: `${sdMetadata.image.steps}`,
+              inline: true,
+            },
+            {
+              name: 'CFG scale',
+              value: `${sdMetadata.image.cfg_scale}`,
+              inline: true,
+            },
+            {
+              name: 'Postprocessing',
+              value:
+                sdMetadata.image.postprocessing
+                  ?.map(
+                    (k) =>
+                      `${k.type}${k.strength ? ` ${k.strength}` : ''}${
+                        k.scale ? ` ${k.scale}x` : ''
+                      }`
+                  )
+                  .join('\n') ?? 'None detected',
+              inline: true,
+            }
+          )
+          .setThumbnail(png.url)
+          .setFooter({
+            text: `Generated with ${sdMetadata.app_id} ${sdMetadata.app_version}`,
+          })
+          .setColor(0x38bdf8),
+      ],
+    });
+  } else if (data['parameters']) {
+    const parameters = (data.parameters as string).split('\n').filter(Boolean);
+
+    const prompt = parameters[0];
+    const negativePrompt = parameters
+      .slice(1)
+      .filter((k) => k.startsWith('Negative prompt:'))[0];
+    const options = parameters[parameters.length - 1]
+      .split(', ')
+      .map((k) => k.split(': '));
+
+    await e.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle('Stable Diffusion metadata')
+          .setFields(
+            {
+              name: 'Prompt',
+              value: prompt,
+            },
+            ...(negativePrompt
+              ? [
+                  {
+                    name: 'Negative prompt',
+                    value: negativePrompt,
+                  },
+                ]
+              : []),
+            {
+              name: 'Extras',
+              value: data['extras'] ?? 'None detected',
+            },
+            ...options.map((opt) => ({
+              name: opt[0],
+              value: opt[1],
+              inline: true,
+            }))
+          )
+          .setThumbnail(png.url)
+          .setFooter({
+            text: `Generated with A1111`,
+          })
+          .setColor(0x38bdf8),
+      ],
+    });
+  }
 };
