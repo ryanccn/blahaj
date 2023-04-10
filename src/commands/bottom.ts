eb7bfa023ee08fab6557179cf3c7043102bfb0cf
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-04-10 20:31:16 +0800

    fix(bottom): better error message for invalid encoded text

--- src/commands/bottom.ts
+++ src/commands/bottom.ts
@@ -1,11 +1,27 @@
 import { encode, decode } from 'bottomify';
-import { SlashCommand } from './_types';
+import type { SlashCommand } from './_types';
 
 export const bottomCommand: SlashCommand = async (i) => {
   const subcmd = i.options.getSubcommand();
   const msg = i.options.getString('content', true);
-  await i.reply({
-    content: subcmd === 'encode' ? encode(msg) : decode(msg),
-    allowedMentions: { parse: [] },
-  });
+
+  try {
+    await i.reply({
+      content: subcmd === 'encode' ? encode(msg) : decode(msg),
+      allowedMentions: { parse: [] },
+    });
+  } catch (error: unknown) {
+    if (
+      error instanceof TypeError &&
+      error.message.includes('Invalid bottom text')
+    ) {
+      await i.reply({
+        content: 'You did not provide a properly encoded bottom text!',
+        ephemeral: true,
+      });
+      return;
+    }
+
+    throw error;
+  }
 };
