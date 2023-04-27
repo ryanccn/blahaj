f0dcbd34be09a21690f1533f76f7a23cbc8c15d9
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-04-27 16:47:45 +0800

    feat: initial backend with modal

---
+++ stable_diffusion/config.py
@@ -0,0 +1,5 @@
+HF_MODEL_ID = "NoCrypt/SomethingV2_2"
+
+TI_EMBEDDINGS = {
+    "EasyNegative.safetensors": "https://huggingface.co/datasets/gsdf/EasyNegative/resolve/main/EasyNegative.safetensors"
+}
---
+++ stable_diffusion/download.py
@@ -0,0 +1,52 @@
+def download_file(url: str, path: str):
+    import requests
+
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+
+
+def download_model():
+    from diffusers import StableDiffusionPipeline
+    from .config import HF_MODEL_ID
+    import torch
+    import gc
+
+    _ = StableDiffusionPipeline.from_pretrained(
+        HF_MODEL_ID,
+        torch_dtype=torch.float16,
+        cache_dir="/root/cache/diffusers",
+        safety_checker=None,
+    )
+    _ = None
+
+    gc.collect()
+
+
+def download_realesrgan():
+    download_file(
+        "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth",
+        "/root/cache/realesrgan/realesr-general-x4v3.pth",
+    )
+    download_file(
+        "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-wdn-x4v3.pth",
+        "/root/cache/realesrgan/realesr-general-wdn-x4v3.pth",
+    )
+
+
+def download_embeddings() -> list[str]:
+    import os
+    from .config import TI_EMBEDDINGS
+
+    ret = []
+
+    for name in TI_EMBEDDINGS:
+        embeddings_path = os.path.join("/root/cache/embeddings", name)
+        if not os.path.exists(embeddings_path):
+            download_file(TI_EMBEDDINGS[name], embeddings_path)
+        ret.append(embeddings_path)
+
+    return ret
