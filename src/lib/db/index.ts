c3f46c13f52b4b779297ef0a766865fffbfa921a
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-02 16:49:32 +0800

    perf: use LRU cache for short term config caching

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,9 @@
 import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
+
 import type { Database, GuildConfig } from "./types";
+
+import { LRUCache } from "lru-cache";
 
 const pool = new Pool({
 	connectionString: process.env.POSTGRES_URL,
@@ -9,7 +12,20 @@ const dialect = new PostgresDialect({ pool });
 
 export const db = new Kysely<Database>({ dialect });
 
+const guildConfigCache = new LRUCache<string, GuildConfig>({
+	max: 500,
+	maxSize: 5000,
+	sizeCalculation: (value) => {
+		return JSON.stringify(value).length;
+	},
+	ttl: 1000,
+	allowStale: false,
+});
+
 export const getGuildConfig = async (guildId: string): Promise<GuildConfig> => {
+	const cacheValue = guildConfigCache.get(guildId);
+	if (cacheValue) return cacheValue;
+
 	const data = await db.selectFrom("guild_config").selectAll().where("guild", "=", guildId).executeTakeFirst();
 
 	if (!data) {
@@ -29,5 +45,6 @@ export const getGuildConfig = async (guildId: string): Promise<GuildConfig> => {
 		return getGuildConfig(guildId);
 	}
 
+	guildConfigCache.set(guildId, data);
 	return data;
 };
