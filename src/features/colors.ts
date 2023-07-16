7e9f4270f17381cc53592e17923deb2b5720f2d6
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-16 19:43:59 +0800

    fix(colors): improve regex and normalize embed color

--- src/features/colors.ts
+++ src/features/colors.ts
@@ -1,7 +1,7 @@
 import { AttachmentBuilder, EmbedBuilder, type Message } from "discord.js";
 import sharp from "sharp";
 
-const hexRegex = /#([\da-f]+)/g;
+const hexRegex = /#(([\da-f]{8})|([\da-f]{6})|([\da-f]{3,4}))/g;
 
 export const handleColors = async (message: Message) => {
 	const hexes = message.content.matchAll(hexRegex);
@@ -9,21 +9,28 @@ export const handleColors = async (message: Message) => {
 	const embeds: EmbedBuilder[] = [];
 	const files: AttachmentBuilder[] = [];
 
-	for (const hex of hexes) {
-		const image = await sharp({ create: { width: 256, height: 256, background: hex[0], channels: 4 } })
+	for (const [fullStr, hex] of hexes) {
+		const image = await sharp({ create: { width: 256, height: 256, background: fullStr, channels: 4 } })
 			.png()
 			.toBuffer();
 
-		files.push(new AttachmentBuilder(image).setName(`hex-${hex[1]}.png`));
-		embeds.push(
-			new EmbedBuilder()
-				.setTitle(hex[0])
-				.setColor(Number.parseInt(hex[1], 16))
-				.setThumbnail(`attachment://hex-${hex[1]}.png`)
-		);
+		const normalizedHex =
+			hex.length === 8
+				? hex.slice(0, 6)
+				: hex.length === 6
+				? hex
+				: [...hex]
+						.map((k) => `${k}${k}`)
+						.join("")
+						.slice(0, 6);
+
+		const intColor = Number.parseInt(normalizedHex, 16);
+
+		files.push(new AttachmentBuilder(image).setName(`hex-${hex}.png`));
+		embeds.push(new EmbedBuilder().setTitle(fullStr).setColor(intColor).setThumbnail(`attachment://hex-${hex}.png`));
 	}
 
 	if (embeds.length > 0) {
-		await message.reply({ files, embeds });
+		await message.reply({ files, embeds, allowedMentions: { repliedUser: false } });
 	}
 };
