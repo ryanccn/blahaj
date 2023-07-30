d72763318372e71abb6deeda9b48f4fe277a3b3e
uncenter <47499684+uncenter@users.noreply.github.com>
2023-07-30 00:14:13 -0400

    feat(colors): accept CSS <named-color> keywords

--- src/commands/utils/color.ts
+++ src/commands/utils/color.ts
@@ -1,6 +1,8 @@
 import { AttachmentBuilder, EmbedBuilder } from "discord.js";
 import sharp from "sharp";
 import { SlashCommand } from "../_types";
+// @ts-expect-error CJS
+import cssColorKeywords from "css-color-keywords";
 
 const hexRegex = /^#(([\dA-Fa-f]{8})|([\dA-Fa-f]{6})|([\dA-Fa-f]{3,4}))$/;
 export const colorCommand: SlashCommand = async (i) => {
@@ -16,6 +18,9 @@ export const colorCommand: SlashCommand = async (i) => {
 	} else if (hexRegex.test("#" + input)) {
 		fullString = "#" + input;
 		hexOnly = input;
+	} else if (Object.prototype.hasOwnProperty.call(cssColorKeywords as { [key: string]: string }, input)) {
+		fullString = cssColorKeywords[input];
+		hexOnly = cssColorKeywords[input].slice(1);
 	} else {
 		await i.editReply("Invalid color provided!");
 		return;
