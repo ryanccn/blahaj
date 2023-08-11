0ab356226f5cecc20b997a5746701cc23f82b2dc
uncenter <47499684+uncenter@users.noreply.github.com>
2023-08-11 13:39:06 +0200

    feat(colors): accept more formats (#54)

--- src/commands/utils/color.ts
+++ src/commands/utils/color.ts
@@ -1,47 +1,43 @@
 import { AttachmentBuilder, EmbedBuilder } from "discord.js";
 import sharp from "sharp";
 import { SlashCommand } from "../_types";
+import { ColorTranslator } from "colortranslator";
 
-const hexRegex = /^#(([\dA-Fa-f]{8})|([\dA-Fa-f]{6})|([\dA-Fa-f]{3,4}))$/;
+const customColors: {
+	[key: string]: string;
+} = {
+	transparent: "#00000000",
+};
+
 export const colorCommand: SlashCommand = async (i) => {
 	await i.deferReply();
 	const input = i.options.getString("color", true);
 
-	let fullString: string;
-	let hexOnly: string;
+	let fullColor: ColorTranslator;
 
-	if (hexRegex.test(input)) {
-		fullString = input;
-		hexOnly = input.slice(1);
-	} else if (hexRegex.test("#" + input)) {
-		fullString = "#" + input;
-		hexOnly = input;
+	if (input in customColors) {
+		fullColor = new ColorTranslator(customColors[input]);
 	} else {
-		await i.editReply("Invalid color provided!");
-		return;
+		try {
+			fullColor = new ColorTranslator(input);
+		} catch {
+			try {
+				fullColor = new ColorTranslator("#" + input);
+			} catch {
+				await i.editReply("Invalid color provided!");
+				return;
+			}
+		}
 	}
 
-	const image = await sharp({ create: { width: 256, height: 256, background: fullString, channels: 4 } })
+	const image = await sharp({ create: { width: 256, height: 256, background: fullColor.HEXA, channels: 4 } })
 		.png()
 		.toBuffer();
 
-	const normalizedHex =
-		hexOnly.length === 8
-			? hexOnly.slice(0, 6)
-			: hexOnly.length === 6
-			? hexOnly
-			: [...hexOnly]
-					.map((k) => `${k}${k}`)
-					.join("")
-					.slice(0, 6);
-
-	const intColor = Number.parseInt(normalizedHex, 16);
-
-	const file = new AttachmentBuilder(image).setName(`hex-${hexOnly}.png`);
+	const file = new AttachmentBuilder(image).setName(`hex-${fullColor.HEXA.slice(1)}.png`);
 	const embed = new EmbedBuilder()
-		.setTitle(fullString)
-		.setColor(intColor)
-		.setThumbnail(`attachment://hex-${hexOnly}.png`);
-
+		.setTitle(fullColor.HEXA)
+		.setColor(Number.parseInt(fullColor.HEX.slice(1), 16))
+		.setThumbnail(`attachment://hex-${fullColor.HEXA.slice(1)}.png`);
 	await i.editReply({ files: [file], embeds: [embed] });
 };
